3d438e7d8ba1af341974b1bb159ae01ac2c8b3bd
lucasvieiraeng325 <118945962+lucasvieiraeng325@users.noreply.github.com>
2026-07-27 11:11:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+from fastapi import FastAPI
 
+app = FastAPI(title="Chatbot WhatsApp IA")
+
+
+@app.get("/")
+def home():
+    return {
+        "status": "online",
+        "mensagem": "Chatbot funcionando!"
+    }
+
+
+@app.get("/health")
+def health():
+    return {
+        "status": "ok"
+    }
